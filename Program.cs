721336ac83c373503c1c164fd308123d34928ab2
Stephen Chryn <SRCthird@gmail.com>
2024-05-24 13:17:16 -0400

    Add DatabaseConnection to Service Scope

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using RESTful.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddScoped<IDatabaseConnection, DatabaseConnection>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
