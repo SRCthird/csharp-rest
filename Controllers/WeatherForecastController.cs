565901565b5654eb72e8aba18189d0616e0c66e5
Stephen Chryn <SRCthird@gmail.com>
2024-05-24 13:21:27 -0400

    Create dynamic summary

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,10 +8,19 @@ namespace RESTful.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+  private string GetWeatherSummary(int temperatureC)
+  {
+      if (temperatureC <= 0) return "Freezing";
+      if (temperatureC <= 5) return "Bracing";
+      if (temperatureC <= 10) return "Chilly";
+      if (temperatureC <= 15) return "Cool";
+      if (temperatureC <= 20) return "Mild";
+      if (temperatureC <= 25) return "Warm";
+      if (temperatureC <= 30) return "Balmy";
+      if (temperatureC <= 35) return "Hot";
+      if (temperatureC <= 40) return "Sweltering";
+      return "Scorching";
+  }
 
     private readonly ILogger<WeatherForecastController> _logger;
 
